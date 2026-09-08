ece1f3915f4d295e7e8c971af32bae002c2454e1
cls <gursimransinghthukral@gmail.com>
2026-09-08 20:53:45 +0530

    vm runner fix

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { verifyTokenOptional } = require('./middleware/authMiddleware');
 
 const vmProxy = createProxyMiddleware({
-  target: process.env.VM_RUNNER_URL || 'https://vm-runner-61rp.onrender.com',
+  target: process.env.VM_RUNNER_URL || 'http://localhost:8080',
   changeOrigin: true,
   pathRewrite: (path, req) => req.originalUrl || req.url,
   ws: true, // proxy websockets
